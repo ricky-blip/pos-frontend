9bd7b93787eed23f88394ce86a4e6a944bcf5510
ricky-blip <ricky.rinaldy77@gmail.com>
2026-03-11 20:47:09 +0700

    new project react POS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-[#4C3BCF]">
+      <h1 className="text-white text-5xl font-bold">
+        POS System
+      </h1>
+    </div>
+  )
+}
+
+export default App
